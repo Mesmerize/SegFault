5e07b4cb6c632fcf474ae93167ff59b220ad19b9
Brandon Luna Rodriguez <disrespectings@gmail.com>
2022-02-27 02:59:22 -0800

    Finally added parsing functionality to the sudoku grid and stdin reads it appropriately

--- sudoku.c
+++ sudoku.c
@@ -7,6 +7,9 @@
 #include <ctype.h>
 #include <getopt.h>
 
+void readSudoku(int x[][9], FILE *in);
+void printSudoku(int x[][9]);
+
 /* These are the only two global variables allowed in your program */
 static int verbose = 0;
 static int use_fork = 0;
@@ -55,28 +58,62 @@ int main(int argc, char *argv[])
     // }
 
     // printf("Test");
-
-    // Building file opening and closing logic
-
-    // If user enters less than 2 arguments it will give out an error
-    if (argc < 2)
-    {
-        fprintf(stderr, "CLI Usage: filename [input-file-of-grid]\n");
-        exit(EXIT_FAILURE);
-    }
-
+    
     // Initializing sudoku grid to parse file grid
     int sudoku_grid[9][9];
-    int numGrids;
 
-    // Opening File
-    FILE *openFile = fopen(argv[1], "r");
-    if (!openFile)
-    {
-        fprintf(stderr, "Could not open \"%s\" for reading! \n", argv[1]);
-    } else {
-        printf("\"%s\" opened!\n", argv[1]);
+    if(argc == 1) {
+        printf("File successfully opened!\n");
     }
 
+    readSudoku(sudoku_grid, stdin);
+    printSudoku(sudoku_grid);
+
     return 0;
+}
+
+void readSudoku(int x[][9], FILE *in) {
+    fseek(in, -1, SEEK_CUR); // Seek to start off the current position of the file ptr
+
+    char entry;
+    int i, j, totalVals = 0;
+
+	while((fread(&entry, 1, 1, in)) > 0 && totalVals < 81){ // Read 81 digits from stdin file
+		if(entry != '\n'){ // Ignore newline
+			if(isdigit(entry)){
+				++totalVals;
+				x[i][j] = entry - '0'; // Store integer representation
+				++j;
+				if(j == 9){
+					j = 0;
+					++i;
+				}
+			}
+		}
+	}
+}
+
+void printSudoku(int x[][9]){
+    int i=0, j=0; // i = rows, j = cols
+    for (i=0; i<9; i++) {
+        for (j=0; j<9; j++) {
+            // if we are on the third or fifth number of sub-grid 
+            // we make a space between nums
+            if (2 == j || 5 == j) {
+                printf("%d   ", x[i][j]);
+            } 
+            // if we are on the last num of row we make a space
+            else if (8 == j) {
+                printf("%d\n", x[i][j]);
+            } 
+            // anything else we make a space
+            else {
+                printf("%d ", x[i][j]);
+            }
+        }
+        // if we are on row 3 or row 5 we make a space
+        if (2 == i || 5 == i) {
+            printf("\n");
+        }
+    }
 }
